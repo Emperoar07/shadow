683c6dfc45141fd8b28c336f8bdceaac5363840d
Emperoar07 <bolajilateef07@gmail.com>
2026-03-22 15:09:54 +0100

    Revert "Fix collateral modal layering and trading footer link"
    This reverts commit d1db93703fdc901f315b88a12a352210805c8f5e.

--- app/src/pages/app.tsx
+++ app/src/pages/app.tsx
@@ -302,9 +302,6 @@ export default function TradingAppPage() {
                 </a>{" "}
                 | Built on Solana
               </p>
-              <Link href="/docs#privacy-policy" className="text-gray-400 hover:text-gray-200 transition-colors">
-                Privacy Policy
-              </Link>
             </div>
           </footer>
         </div>
